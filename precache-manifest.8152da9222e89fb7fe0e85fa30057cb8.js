7b81a1eca6be348a5c3f42e842abf513616635e8
Spike <jchoi166@gmail.com>
2018-12-18 19:34:17 -0800

    Updates

---
+++ precache-manifest.8152da9222e89fb7fe0e85fa30057cb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0885653aa629ee936e8e",
+    "url": "/newclicky/static/js/runtime~main.0885653a.js"
+  },
+  {
+    "revision": "3c4b9638da36e6d1b199",
+    "url": "/newclicky/static/js/main.3c4b9638.chunk.js"
+  },
+  {
+    "revision": "7d555d5cbafbd505882b",
+    "url": "/newclicky/static/js/1.7d555d5c.chunk.js"
+  },
+  {
+    "revision": "3c4b9638da36e6d1b199",
+    "url": "/newclicky/static/css/main.3e4092a6.chunk.css"
+  },
+  {
+    "revision": "8f08288d394f13a4330835ddf1b55482",
+    "url": "/newclicky/index.html"
+  }
+];
